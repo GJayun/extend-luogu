e648de1c494448a23a48e186ce6ea14f533cb841
GJayun <59090605+GJayun@users.noreply.github.com>
2022-01-12 10:55:31 +0800

    fixed

--- extend-luogu.user.js
+++ extend-luogu.user.js
@@ -441,11 +441,11 @@ const mod = {
             </svg>
             `
             let $board = $("#exlg-board")
-            if (! $board.length) 
+            if (! $board.length)
                 $board = $(`
                     <div class="lg-article" id="exlg-board" exlg="exlg"><h2>${icon_b} &nbsp;&nbsp;${GM_info.script.version}</h2></div>
                 `)
-                    .prependTo(".lg-right.am-u-md-4"), 
+                    .prependTo(".lg-right.am-u-md-4"),
                 $board[0].firstChild.style["font-size"]="1em"
             func({ ...arg, $board: $(`<div></div>`).appendTo($board) })
         }, styl
@@ -595,7 +595,7 @@ mod.reg_hook_new("dash-bridge", "控制桥", "@/.*", {
 }, ({ msto, args }) => {
     const create_window = ! args.parent().hasClass("mobile-nav-container")
     // console.log(create_window)
-    const $spn = $(`<span id="exlg-dash-window" class="exlg-window" style="display: none;width: 300px;"></span>`).css("left", "1240px")
+    const $spn = $(`<span id="exlg-dash-window" class="exlg-window" style="display: none; width: 300px; top: 205px;"></span>`).css("left", "-300px")
     const $btn = $(`<div id="exlg-dash" exlg="exlg"><svg data-v-78704ac9="" data-v-303bbf52="" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user-cog" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="svg-inline--fa fa-cog fa-w-20"><path data-v-78704ac9="" data-v-303bbf52="" fill="currentColor" d="M487.4 315.7l-42.6-24.6c4.3-23.2 4.3-47 0-70.2l42.6-24.6c4.9-2.8 7.1-8.6 5.5-14-11.1-35.6-30-67.8-54.7-94.6-3.8-4.1-10-5.1-14.8-2.3L380.8 110c-17.9-15.4-38.5-27.3-60.8-35.1V25.8c0-5.6-3.9-10.5-9.4-11.7-36.7-8.2-74.3-7.8-109.2 0-5.5 1.2-9.4 6.1-9.4 11.7V75c-22.2 7.9-42.8 19.8-60.8 35.1L88.7 85.5c-4.9-2.8-11-1.9-14.8 2.3-24.7 26.7-43.6 58.9-54.7 94.6-1.7 5.4.6 11.2 5.5 14L67.3 221c-4.3 23.2-4.3 47 0 70.2l-42.6 24.6c-4.9 2.8-7.1 8.6-5.5 14 11.1 35.6 30 67.8 54.7 94.6 3.8 4.1 10 5.1 14.8 2.3l42.6-24.6c17.9 15.4 38.5 27.3 60.8 35.1v49.2c0 5.6 3.9 10.5 9.4 11.7 36.7 8.2 74.3 7.8 109.2 0 5.5-1.2 9.4-6.1 9.4-11.7v-49.2c22.2-7.9 42.8-19.8 60.8-35.1l42.6 24.6c4.9 2.8 11 1.9 14.8-2.3 24.7-26.7 43.6-58.9 54.7-94.6 1.5-5.5-.7-11.3-5.6-14.1zM256 336c-44.1 0-80-35.9-80-80s35.9-80 80-80 80 35.9 80 80-35.9 80-80 80z" class=""></path></svg> 设置</div>|
     </></div>`)
         .prependTo(args)
@@ -623,7 +623,7 @@ mod.reg_hook_new("dash-bridge", "控制桥", "@/.*", {
     else $btn.on("click", _jump_settings)
 
     if (create_window) {
-        $spn.prependTo(args)
+        $spn.prependTo(".dropdown")
         let mondsh = false, monbrd = false
         $btn.on("mouseenter", () => {
             mondsh = true, $spn.show()
@@ -640,7 +640,7 @@ mod.reg_hook_new("dash-bridge", "控制桥", "@/.*", {
                 monbrd = false
                 if (! mondsh) $spn.hide()
             })
-  
+
         $(`<h2 align="center" style="margin-top: 5px;margin-bottom: 10px;"><svg xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 0 136.14 30.56">
         <g transform="translate(1.755, 0)" fill="#00a0d8">
             <g>
@@ -725,7 +725,7 @@ mod.reg_hook_new("dash-bridge", "控制桥", "@/.*", {
                 $(`<button id="exlg-badge-btn" style="background-color: #ccc;border-color: #666;" class="exlg-windiv-btn" disabled="disabled">提交</button>`).appendTo($span)
             }
         })
-        
+
 
     }
 }, (e) => {
@@ -845,8 +845,8 @@ mod.reg_hook_new("dash-bridge", "控制桥", "@/.*", {
     .exlg-difficulty-color.color-7 { color: rgb(14, 29, 105)!important; }
 
     .exlg-window {
-        position: fixed;
-        top: 265px;
+        position: absolute;
+        top: 35px;
         left: 0px;
         z-index: 65536;
         display: none;
@@ -867,7 +867,7 @@ mod.reg_hook_new("dash-bridge", "控制桥", "@/.*", {
         display: inline-block;
     }
     .exlg-windiv-btn {
-        
+
         font-size: 0.9em;
         /*padding: 0.313em 1em;*/
         display: inline-block;
@@ -883,7 +883,7 @@ mod.reg_hook_new("dash-bridge", "控制桥", "@/.*", {
         border-radius: 5px;
         border: 1px solid;
         margin: 5px 5px;
-        
+
     }
 `)
 
@@ -925,12 +925,12 @@ mod.reg("exlg-dialog-board", "exlg 公告板", "@/.*", {
      </div>
      <div class="exlg-dialog-body">
          <div id="exlg-dialog-content">
- 
+
          </div>
      </div>
     </div>
    </div>`).appendTo($(document.body))
-    const wait_time = {"0s": 0, ".2s": 100, ".25s": 250, ".4s": 400 }[msto.animation_speed] 
+    const wait_time = {"0s": 0, ".2s": 100, ".25s": 250, ".4s": 400 }[msto.animation_speed]
     const wrapper = $wrapper[0], container = wrapper.firstElementChild,
         header = container.firstElementChild.firstElementChild.firstElementChild, content = container.lastElementChild.firstElementChild,
         close_btn = container.firstElementChild.lastElementChild
@@ -1346,7 +1346,7 @@ mod.reg_hook_new("user-problem-color", "题目颜色数量和比较", "@/user/[0
                 }
             })
         }
-        
+
         $("#exlg-problem-count-1").html(`<span class="exlg-counter" exlg="exlg">${ ta.length } <> ${ my.length } : ${same}`
             + `<i class="exlg-icon exlg-info" name="ta 的 &lt;&gt; 我的 : 相同"></i></span>`)
     }
